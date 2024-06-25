75fe7591c083488bbadb6f1482e88314e09d5e16
Aroze <admin@aroze.me>
2024-06-25 21:10:53 +0100

    feat: config rework

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import {ConfigLoader} from "./config/ConfigLoader";
-
-const config = new ConfigLoader("config.toml")
-console.log(config.getConfig().authorization.discord_token)
---
+++ src/snuggles.ts
@@ -0,0 +1,33 @@
+import {Client, GatewayIntentBits} from "discord.js";
+import { Logger, ILogObj } from "tslog";
+import config from "./config/Config";
+import {PingCommand} from "./command/impl/PingCommand";
+import {deployCommands} from "./util/deployCommands";
+
+export const logger: Logger<ILogObj> = new Logger();
+
+const intents = [
+    GatewayIntentBits.Guilds
+];
+
+async function registerCommands(client: Client) {
+    let now = new Date();
+
+    const commands = [
+        new PingCommand()
+    ];
+
+    await deployCommands(client, commands, true, "1246242269154246796")
+    logger.info(`Registered commands in ${new Date().getTime() - now.getTime()}ms`);
+}
+
+(async () => {
+    const client = new Client({
+        intents
+    });
+
+    await client.login(config().authorization.discord_token)
+    logger.info(`Logged in as ${client.user!.tag}`);
+
+    await registerCommands(client);
+})();
